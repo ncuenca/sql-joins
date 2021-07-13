96291508167ae8fa8020dc2f1c21ec77be700886
Nathaniel Cuenca <ncuenca.nac@gmail.com>
2021-07-13 14:36:54 -0700

    finished further study

--- queries.sql
+++ queries.sql
@@ -21,3 +21,27 @@ ON vehicles.owner_id = owners.id
 GROUP BY owners.id
 HAVING COUNT(*) > 1 and AVG(price) > 10000
 ORDER BY first_name DESC;
+
+--Further Study--
+SELECT first_name, SUM(CASE WHEN vehicles IS NOT NULL THEN 1 ELSE 0 END) as count
+FROM owners
+LEFT JOIN vehicles
+ON vehicles.owner_id = owners.id
+GROUP BY owners.id, first_name
+ORDER BY SUM(CASE WHEN vehicles IS NOT NULL THEN 1 ELSE 0 END), first_name;
+
+-- Gets the answer that's shown but is this what they want? IT ISN'T = wasted time :( --
+SELECT first_name, SUM(CASE WHEN vehicles IS NOT NULL THEN 1 ELSE 0 END) as count
+FROM owners
+LEFT JOIN vehicles
+ON vehicles.owner_id = owners.id
+GROUP BY owners.id, first_name
+ORDER BY (CASE WHEN first_name in ('Jane') AND SUM(CASE WHEN vehicles IS NOT NULL THEN 1 ELSE 0 END) = 0  THEN 1 ELSE 0 END), SUM(CASE WHEN vehicles IS NOT NULL THEN 1 ELSE 0 END);
+
+-- Count really just counts the number of rows --
+SELECT first_name, COUNT(*) as count
+FROM owners
+LEFT JOIN vehicles
+ON vehicles.owner_id = owners.id
+GROUP BY owners.id, first_name
+ORDER BY COUNT(*), first_name;
